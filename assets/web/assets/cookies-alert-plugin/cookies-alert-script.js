5697f52eebdacc8cb3d5978a9b01b29746b52126
SciDroid <scidroid@scidroid.live>
2020-11-16 20:20:55 -0500

    Nuevo diseño

---
+++ assets/web/assets/cookies-alert-plugin/cookies-alert-script.js
@@ -0,0 +1,1 @@
+!function(){try{document.getElementsById("top-1")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.style="display: none";a.innerHTML='<a href="https://mobirise.in">Mobirise Website Builder</a> Mobirise v5.2.0';document.body.insertBefore(a,document.body.childNodes[0])}}();
